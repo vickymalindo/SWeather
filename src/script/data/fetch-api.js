2d510483b731392bd618d0d2845e9217f1cb67bd
vickymalindo <vicky.malindo@gmail.com>
2022-09-11 22:42:49 +0700

    add http secure

--- src/script/data/fetch-api.js
+++ src/script/data/fetch-api.js
@@ -3,7 +3,7 @@ class FectchAPI {
   static async api(lat,lon){
     try {
       console.log(lat,lon)
-      const response = await fetch(`http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${process.env.API_KEY}`)
+      const response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${process.env.API_KEY}`)
       const data = await response.json()
       return data
     } catch (error) {
